32acd80fc681ca3e22657217c15ac05e89d0736b
USUARIO <78711175+pgap22@users.noreply.github.com>
2023-09-03 01:44:32 -0600

    me confundi en el merge

--- src/main.jsx
+++ src/main.jsx
@@ -9,18 +9,15 @@ import { router } from "./router";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { SessionProvider } from "./context/Session";
 import ServerStatus from "./pages/serverStatus";
-import { TranaslationProvider } from "./context/Translate";
 
 const queryClient = new QueryClient();
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ServerStatus>
     <SessionProvider>
-      <TranaslationProvider>
-        <QueryClientProvider client={queryClient}>
-            <RouterProvider router={router} />
-        </QueryClientProvider>
-      </TranaslationProvider>
+      <QueryClientProvider client={queryClient}>
+          <RouterProvider router={router} />
+      </QueryClientProvider>
     </SessionProvider>
   </ServerStatus>
 );
