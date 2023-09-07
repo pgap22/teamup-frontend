14ac2c40f4c10b7022698c3985c881a64eda1eb5
Francisco Josue <francisco.sv.xd@gmail.com>
2023-09-06 19:59:54 -0600

    tabla dato fila

--- src/components/ui/Tabla/TablaDatoFila.jsx
+++ src/components/ui/Tabla/TablaDatoFila.jsx
@@ -2,15 +2,14 @@ import clsx from "clsx";
 
 const TablaDatoFila = ({ datos, filas }) => {
   return (
-    <tr
-      className={clsx(
-        "last:rounded-b-md",
-        "even:bg-gray-200",
-        "w-full"
-      )}
-    >
+    <tr className={clsx("last:rounded-b-md", "even:bg-gray-200", "w-full")}>
       {datos.map((fila, i) => (
-        <td className="text-lg whitespace-normal px-4 py-5" style={{ minWidth: `${filas[i].length}ch` }} key={i + "-dato"}>
+        <td
+          title={fila}
+          className="px-4 py-5 text-lg truncate"
+          style={{ maxWidth: `${filas[i].length + 5}ch` }}
+          key={i + "-dato"}
+        >
           {fila}
         </td>
       ))}
